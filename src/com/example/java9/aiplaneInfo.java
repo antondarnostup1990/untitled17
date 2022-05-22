fe7d3891c1fa71e0d9a091899f5ea1233aa62017
Anton.Darnostup <antondarnostup1990@gmail.com>
2022-05-22 23:06:43 +0300

    Initial commit

---
+++ src/com/example/java9/aiplaneInfo.java
@@ -0,0 +1,21 @@
+package com.example.java9;
+
+public class aiplaneInfo {
+
+    private static final int A320 = 179;
+    private static final int F = 15;
+    public static Integer Jet;
+    public static Integer Airbus;
+    public static Integer Boeing;
+
+    public static class airplane {
+
+        public static void main(String[] args) {
+            System.out.println("Тип Самолёта: Jet Модель F - 15 Eagle");
+            System.out.println("Тип Самолёта: Airbus, Модель: A320, Количетсво пассажирских мест: 179");
+            System.out.println("Тип Самолёта: Boeing, Модель: 787 Dreamliner, Пассажирских мест: 250, Бизнесс класса: 30");
+        }
+    }
+}
+
+
